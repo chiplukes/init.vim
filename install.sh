e1e640ff574b9bffde209d7560cc63ccb8c0f7de
chiplukes <chiplukes@hotmail.com>
2018-12-28 14:43:48 -0700

    added mechanism to point vimrc to init.vim

--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 ln -s ~/.config/nvim/cfg/init.vim ~/.config/nvim/init.vim
+ln -s ~/.config/nvim/cfg/.vimrc ~/.vimrc
+
+
